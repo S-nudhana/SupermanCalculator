21a66e49e41b67033a447b124b6b4d719afe8b90
Nudhana Sarutipaisan <snudhana@Nudhanas-MacBook-Pro.local>
2024-02-21 00:02:17 +0700

    Optimize clear func.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const i = 0;
-
 function calculate(){
     var B1 = document.getElementById("B1").value;
     var B2 = document.getElementById("B2").value;
@@ -27,7 +25,7 @@ function calculate(){
             document.getElementById("B2").value = (B1 * P1 * K2)/(K1 * P2);
             document.getElementById("B2").style.color = "red";
         }else if(P1 == ""){
-            document.getElementById("P1").setAttribute().value = (B2 * P2 * K1)/(K2 * B1);
+            document.getElementById("P1").value = (B2 * P2 * K1)/(K2 * B1);
             document.getElementById("P1").style.color = "red";
         }else if(P2 == ""){
             document.getElementById("P2").value = (B1 * P1 * K2)/(B2 * K1);
@@ -43,19 +41,20 @@ function calculate(){
 }
 
 function clear_input(){
-    document.getElementById("B1").value = "";
-    document.getElementById("B1").style.color = "black";
-    document.getElementById("B2").value = "";
-    document.getElementById("B2").style.color = "black";
-    document.getElementById("P1").value = "";
-    document.getElementById("P1").style.color = "black";
-    document.getElementById("P2").value = "";
-    document.getElementById("P2").style.color = "black";
-    document.getElementById("K1").value = "";
-    document.getElementById("K1").style.color = "black";
-    document.getElementById("K2").value = "";
-    document.getElementById("K2").style.color = "black";
+    resetElement("B1");
+    resetElement("B2");
+    resetElement("P1");
+    resetElement("P2");
+    resetElement("K1");
+    resetElement("K2");
 }
+
+function resetElement(id){
+    var element = document.getElementById(id);
+    element.value = "";
+    element.style.color = "black";
+}
+
 function Kelvin(){
     document.getElementById("ndegree1").innerHTML = "K1";
     document.getElementById("ndegree2").innerHTML = "K2";
@@ -68,12 +67,10 @@ function Celcius(){
     document.getElementById("ndegree2").innerHTML = "C2 = ";
     document.getElementById("degree1").innerHTML = "(Celcius)";
     document.getElementById("degree2").innerHTML = "(Celcius)";
-    // i = 273;
 }
 function Farenheit(){
     document.getElementById("ndegree1").innerHTML = "F1 = ";
     document.getElementById("ndegree2").innerHTML = "F2 = ";
     document.getElementById("degree1").innerHTML = "(Farenheit)";
     document.getElementById("degree2").innerHTML = "(Farenheit)";
-    // i = 255;
 }
